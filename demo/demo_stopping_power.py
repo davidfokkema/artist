9c909c64b888727d0e6c33af07c24266563eaa16
David Fokkema <davidfokkema@gmail.com>
2012-03-29 22:02:19 +0200

    Minor cleanup

--- demo/demo_stopping_power.py
+++ demo/demo_stopping_power.py
@@ -30,6 +30,9 @@ def main():
     mu_beta_gamma = mu_p / mu_mass
     mu_loss = mu_data[:, 1]
 
+
+    # Matplotlib
+
     plt.figure()
     plt.loglog(e_beta_gamma, e_loss, label="e")
     plt.loglog(mu_beta_gamma, mu_loss, label=r"$\mu$")
@@ -38,6 +41,9 @@ def main():
     plt.legend()
     plt.savefig('stopping-power-mpl.pdf')
 
+
+    # Artist
+
     plot = GraphArtist(axis='loglog', width=r'.5\linewidth')
     plot.plot(e_beta_gamma, e_loss, mark=None)
     plot.plot(mu_beta_gamma, mu_loss, mark=None)
@@ -45,13 +51,14 @@ def main():
     plot.set_ylabel(r'Stopping Power $\left[\si{\mega\electronvolt'
                                               r'\centi\meter\squared'
                                               r'\per\gram}\right]$')
+
     plot.add_pin_at_xy(e_beta_gamma, e_loss, 'e', location='below right',
                        relative_position=.8)
     plot.add_pin_at_xy(mu_beta_gamma, mu_loss, r'$\mu$', 'above left')
+
     plot.set_xlimits(1e-2, 1e8)
     plot.set_ylimits(min=1)
     plot.set_logxticks(range(-2, 9, 2))
-
     plot.save('stopping-power')
 
 
