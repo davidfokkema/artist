4a15aabdf3795827c540cc2d4e90dee37bf83801
David Fokkema <davidfokkema@gmail.com>
2012-05-30 22:12:38 +0200

    Added multiplot demo to document

--- demo/demo_multiplot.py
+++ demo/demo_multiplot.py
@@ -7,17 +7,19 @@ import artist
 def main():
     np.random.seed(1)
 
-    graph = artist.MultiPlot(2, 3, width=r'.5\linewidth')
+    graph = artist.MultiPlot(2, 3, width=r'.35\linewidth',
+                             height=r'.35\linewidth')
     x = np.linspace(-pi, pi)
     graph.plot(0, 1, x, sin(x), mark=None)
     graph.add_pin(0, 1, '$\sin(x)$', relative_position=.5)
     graph.plot(1, 0, x, cos(x), mark=None)
-    graph.add_pin_at_xy(1, 0, 1, .5, '$\cos(x)$')
+    graph.add_pin_at_xy(1, 0, 1, .5, '$\cos(x)$', use_arrow=True)
     graph.plot(1, 2, x, tan(x), mark=None)
 
     x = np.random.normal(size=1000)
     n, bins = np.histogram(x, bins=20)
     graph.histogram(0, 0, n, bins)
+    graph.add_pin(0, 0, 'histogram', location='left', relative_position=.5)
 
     x = range(5)
     lower = np.random.uniform(-2, -1, size=5)
@@ -28,7 +30,7 @@ def main():
 
     graph.plot(1, 1, range(5), np.random.normal(size=5))
 
-    graph.save('preview.tex')
+    graph.save('multiplot.tex')
     graph.save_as_pdf('preview.pdf')
 
 
