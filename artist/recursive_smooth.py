51a13f09a1a042a729b4e5dbb663c19415fd2c95
David Fokkema <davidfokkema@gmail.com>
2012-07-09 15:24:46 +0200

    Added log options to smooth data

--- artist/recursive_smooth.py
+++ artist/recursive_smooth.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-def smooth(x, y, degree=1):
+def smooth(x, y, degree=1, logx=False, logy=False):
     """Smooth y-values and return new x, y pair
 
     :param x, y: data values
@@ -21,15 +21,26 @@ def smooth(x, y, degree=1):
     """
     if degree == 0:
         return x, y
-    elif degree == 1:
-        # generate new linearly spaced x-points
-        smoothed_x = np.linspace(min(x), max(x), len(x))
-        # generate new y-points using linear interpolation
-        smoothed_y = np.interp(smoothed_x, x, y)
-        return smoothed_x, smoothed_y
     else:
-        # smooth data by linear interpolation
-        x, y = smooth(x, y, degree - 1)
-        smoothed_x = (x[:-1] + x[1:]) / 2
-        smoothed_y = np.interp(smoothed_x, x, y)
+        if logx:
+            x = np.log10(x)
+        if logy:
+            y = np.log10(y)
+
+        if degree == 1:
+            # generate new linearly spaced x-points
+            smoothed_x = np.linspace(min(x), max(x), len(x))
+            # generate new y-points using linear interpolation
+            smoothed_y = np.interp(smoothed_x, x, y)
+        else:
+            # smooth data by linear interpolation
+            x, y = smooth(x, y, degree - 1)
+            smoothed_x = (x[:-1] + x[1:]) / 2
+            smoothed_y = np.interp(smoothed_x, x, y)
+
+        if logx:
+            smoothed_x = 10 ** smoothed_x
+        if logy:
+            smoothed_y = 10 ** smoothed_y
+
         return smoothed_x, smoothed_y
