59f028c83812261e9150ccff9f224b35ebfa6e05
David Fokkema <davidfokkema@gmail.com>
2012-08-21 15:25:06 +0200

    Improved Science Park Map

--- demo/demo_sciencepark.py
+++ demo/demo_sciencepark.py
@@ -4,13 +4,45 @@ from artist import GraphArtist
 
 
 def main():
-    locations = np.genfromtxt('data/sciencepark-locations.txt',
-                              names=['x', 'y'])
+    locations = np.genfromtxt(
+        'data/SP-DIR-plot_sciencepark_cluster-detectors.txt',
+        names=['x', 'y'])
+    stations = np.genfromtxt(
+        'data/SP-DIR-plot_sciencepark_cluster-stations.txt',
+        names=['id', 'x', 'y'])
 
     graph = GraphArtist()
 
     graph.plot(locations['x'], locations['y'], linestyle=None)
     graph.set_axis_equal()
+
+    locations = ['right'] * len(stations)
+    locations[0] = 'left'
+    locations[5] = 'above right'
+    locations = iter(locations)
+    for num, x, y in stations:
+        graph.add_pin_at_xy(x, y, int(num), location=locations.next(),
+                            use_arrow=False, style='gray,label distance=1ex')
+
+    x = [stations['x'][u] for u in [0, 2, 5]]
+    y = [stations['y'][u] for u in [0, 2, 5]]
+    x.append(x[0])
+    y.append(y[0])
+    graph.plot(x, y, mark=None, linestyle='dashed')
+
+    graph.add_pin_at_xy([x[0], x[1]], [y[0], y[1]], r'\SI{128}{\meter}',
+                        relative_position=.4, location='below right',
+                        use_arrow=False)
+    graph.add_pin_at_xy([x[0], x[2]], [y[0], y[2]], r'\SI{151}{\meter}',
+                        relative_position=.5, location='left',
+                        use_arrow=False)
+    graph.add_pin_at_xy([x[2], x[1]], [y[2], y[1]], r'\SI{122}{\meter}',
+                        relative_position=.5, location='above right',
+                        use_arrow=False)
+
+    graph.set_xlabel(r"Distance [\si{\meter}]")
+    graph.set_ylabel(r"Distance [\si{\meter}]")
+
     graph.save('sciencepark')
 
 
