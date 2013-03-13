c418ca8cb18df810483a9d16df7d639f8b13adff
David Fokkema <davidfokkema@gmail.com>
2013-03-13 17:04:52 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name = "artist",
+    version = "0.9.1b",
+    packages = find_packages(),
+    url = "http://github.com/davidfokkema/artist/",
+    author = "David Fokkema",
+    author_email = "davidfokkema@icloud.com",
+    description = "A plotting library for Python with LaTeX output",
+
+    install_requires = ['jinja2', 'numpy']
+)
