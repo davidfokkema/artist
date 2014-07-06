405f9e6cdcb25756e16dd4e555cf15e506232209
David Fokkema <davidfokkema@gmail.com>
2014-07-06 10:30:31 +0200

    Improved setup.py, for PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup, find_packages
-setup(
-    name = "artist",
-    version = "0.10.0",
-    packages = find_packages(),
-    url = "http://github.com/davidfokkema/artist/",
-    author = "David Fokkema",
-    author_email = "davidfokkema@icloud.com",
-    description = "A plotting library for Python with LaTeX output",
 
-    install_requires = ['jinja2', 'numpy']
+setup(name = "artist",
+      version = "0.10.0",
+      packages = find_packages(),
+      url = "http://github.com/davidfokkema/artist/",
+      bugtrack_url='http://github.com/davidfokkema/artist/issues',
+      license='GPLv3',
+      author = "David Fokkema",
+      author_email = "davidfokkema@icloud.com",
+      description = "A plotting library for Python with LaTeX output",
+      long_description=open('README.rst').read(),
+      keywords=['plots', 'plotting', 'data visualization'],
+      classifiers=['Intended Audience :: Science/Research',
+                   'Intended Audience :: Education',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Topic :: Scientific/Engineering :: Visualization',
+                   'Topic :: Education',
+                   'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'],
+      install_requires = ['jinja2', 'numpy'],
 )
