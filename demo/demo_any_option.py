6e53101dbd054d899b2548ed70faf321063dd460
Arne de Laat <adelaat@nikhef.nl>
2015-07-20 16:12:32 +0200

    Add demo for any_option with MultiPlot

--- demo/demo_any_option.py
+++ demo/demo_any_option.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from artist import Plot
+from artist import Plot, MultiPlot
 
 
 def main():
@@ -20,6 +20,26 @@ def main():
     plot.set_xlabel('Something important')
     plot.set_ylabel('A related thing')
     plot.save('any_option')
+
+    x = np.linspace(.6 * np.pi, 10 * np.pi, 150)
+    y = np.sin(x) / x
+    plot = MultiPlot(1, 2, width=r'.4\linewidth', height=r'.25\linewidth')
+
+    subplot = plot.get_subplot_at(0, 0)
+    subplot.plot(x, y, mark=None)
+
+    subplot = plot.get_subplot_at(0, 1)
+    subplot.plot(x, y, mark=None)
+
+    plot.show_xticklabels_for_all([(0, 0), (0, 1)])
+    plot.show_yticklabels(0, 1)
+    plot.set_axis_options(0, 1, 'yticklabel pos=right, grid=major')
+
+    plot.set_axis_options_for_all(None, r'enlargelimits=false')
+
+    plot.set_xlabel('Something important')
+    plot.set_ylabel('A related thing')
+    plot.save('multi_any_option')
 
 
 if __name__ == '__main__':
