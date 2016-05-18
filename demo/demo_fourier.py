6073ff0cc6965e874d467d71a60c64a5455c3bfa
David Fokkema <davidfokkema@gmail.com>
2016-05-18 16:28:05 +0200

    PEP 8

--- demo/demo_fourier.py
+++ demo/demo_fourier.py
@@ -27,20 +27,18 @@ def fourier(x, N):
     return (4.0 / (math.pi)) * term
 
 
-X     = npy.linspace(0.0, 2 * L, num=1000)
+X = npy.linspace(0.0, 2 * L, num=1000)
 Y_sqr = [square(x) for x in X]
-Y     = lambda n: [fourier(x, n) for x in X]
+Y = lambda n: [fourier(x, n) for x in X]
 
 graph = Plot()
 graph.set_title("Fourier approximation")
 
-graph.plot(x=X, y=Y( 3), linestyle='red'   , mark=None, legend='n=3' )
-graph.plot(x=X, y=Y( 5), linestyle='yellow', mark=None, legend='n=5' )
-graph.plot(x=X, y=Y( 8), linestyle='green' , mark=None, legend='n=8' )
-graph.plot(x=X, y=Y(13), linestyle='blue'  , mark=None, legend='n=13')
-#graph.plot(x=X, y=Y(21), linestyle='violet', mark=None, legend='n=21')
-#graph.plot(x=X, y=Y(34), linestyle='violet', mark=None, legend='n=34')
-graph.plot(x=X, y=Y(55), linestyle='cyan'  , mark=None, legend='n=55')
-graph.plot(x=X, y=Y_sqr, linestyle='black' , mark=None, legend='square')
+graph.plot(x=X, y=Y(3), linestyle='red', mark=None, legend='n=3')
+graph.plot(x=X, y=Y(5), linestyle='yellow', mark=None, legend='n=5')
+graph.plot(x=X, y=Y(8), linestyle='green', mark=None, legend='n=8')
+graph.plot(x=X, y=Y(13), linestyle='blue', mark=None, legend='n=13')
+graph.plot(x=X, y=Y(55), linestyle='cyan', mark=None, legend='n=55')
+graph.plot(x=X, y=Y_sqr, linestyle='black', mark=None, legend='square')
 
 graph.save_as_pdf('plot')
