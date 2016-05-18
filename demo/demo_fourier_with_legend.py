fa22af874fdff7923f368404e3d6dd4e5fabaa28
David Fokkema <davidfokkema@gmail.com>
2016-05-18 19:47:58 +0200

    Add demo without legend, but with labels
    Maybe a bit convoluted, but I like black/white, and labels. Notice how
    the gray lines get darker with increasing number of elements.

--- demo/demo_fourier_with_legend.py
+++ demo/demo_fourier_with_legend.py
@@ -27,6 +27,17 @@ def fourier(x, N):
     return (4.0 / (math.pi)) * term
 
 
+def add_custom_pin(plot, x, y, label, distance='3ex'):
+    ''' Add a pin at the maximum value of a data series (sort of). '''
+    # only use the second quarter, for cosmetic reasons
+    q = int(.25 * len(y))
+    y = y[q:2*q]
+    max_index = q + npy.argmax(y)
+    max_x = x[max_index]
+    plot.add_pin(label, x=max_x, location='above', use_arrow=True,
+                 style='pin distance=%s' % distance)
+
+
 X = npy.linspace(0.0, 2 * L, num=1000)
 Y_sqr = [square(x) for x in X]
 Y = lambda n: [fourier(x, n) for x in X]
@@ -42,3 +53,22 @@ graph.plot(x=X, y=Y(55), linestyle='cyan', mark=None, legend='n=55')
 graph.plot(x=X, y=Y_sqr, linestyle='black', mark=None, legend='square')
 
 graph.save('fourier_with_legend')
+
+graph = Plot()
+graph.set_title("Fourier approximation")
+
+graph.plot(x=X, y=Y(3), mark=None, linestyle='black!20')
+add_custom_pin(graph, X, Y(3), 3)
+graph.plot(x=X, y=Y(5), mark=None, linestyle='black!30')
+add_custom_pin(graph, X, Y(5), 5)
+graph.plot(x=X, y=Y(8), mark=None, linestyle='black!40')
+add_custom_pin(graph, X, Y(8), 8)
+graph.plot(x=X, y=Y(13), mark=None, linestyle='black!60')
+add_custom_pin(graph, X, Y(13), 13, distance='5ex')
+graph.plot(x=X, y=Y(55), mark=None, linestyle='black!80')
+add_custom_pin(graph, X, Y(55), 55)
+graph.plot(x=X, y=Y_sqr, mark=None, linestyle='thick')
+
+graph.set_ylimits(max=1.7)
+
+graph.save('fourier_with_labels')
